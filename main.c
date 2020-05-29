084e54e5c7fd1cf6595d0ae50a562abf7dd5c88b
Alberto Mosconi <pitagora99@gmail.com>
2020-05-29 18:44:02 +0200

    basic loop to get cmd

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
-// edU
-// Alberto Mosconi - 2020
+/* edU */
+/* Alberto Mosconi - 2020 */
 
+#include <stdlib.h>
 #include <stdio.h>
+#define MAX_COMMAND_LENGTH 1024
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello world\n");
+    char raw_input[MAX_COMMAND_LENGTH];
+
+    while (1)
+    {
+        if (NULL == fgets(raw_input, sizeof(raw_input), stdin))
+            break;
+
+        printf("%s", raw_input);
+    }
     return 0;
 }
