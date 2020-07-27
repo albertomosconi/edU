a31ec6178516fa40fdb45ba124630d941ef9eb07
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-07-27 21:23:23 +0200

    change

--- main.c
+++ main.c
@@ -2,14 +2,6 @@
 #include <stdio.h>
 #define MAX_COMMAND_LENGTH 1024
 
-/* MAIN FUNCTIONS */
-void getInput();
-void handleChange();
-void handleDelete();
-void handlePrint();
-void handleUndo();
-void handleRedo();
-
 /* STRUCTURES */
 typedef struct l
 {
@@ -32,42 +24,70 @@ int i1 = 0, i2 = 0;
 char c;
 LineHistory *head = NULL;
 
+/* MAIN FUNCTIONS */
+void getInput();
+void handleChange();
+void handleDelete();
+void handlePrint();
+void handleUndo();
+void handleRedo();
+
+/* HELPER FUNCTIONS*/
+void copyString(char *source, char *destination);
+LineHistory *initializeLineHistory(int i);
+
 int main(int argc, char const *argv[])
 {
     while (1)
     {
         getInput();
-
         switch (c)
         {
         case 'q':
             return 0;
-
         case 'c':
             handleChange();
             break;
-
         case 'd':
             handleDelete();
             break;
-
         case 'p':
             handlePrint();
             break;
-
         case 'u':
             handleUndo();
             break;
-
         case 'r':
             handleRedo();
             break;
-
         default:
             break;
         }
     }
     return 0;
+}
+
+void copyString(char *source, char *destination)
+{
+    for (int c = 0; source[c] != '\0'; ++c)
+        destination[c] = source[c];
+    destination[c] = '\0';
+}
+
+LineHistory *initializeLineHistory(int i)
+{
+    Line line;
+    copyString(".", line.content);
+    line.nextVersion = NULL;
+    line.prevVersion = NULL;
+
+    LineHistory *new = malloc(sizeof(LineHistory));
+    new->index = i;
+    new->nextLine = NULL;
+    new->latestVersion = &line;
+    new->currentVersion = &line;
+
+    return new;
 }
 
 void getInput()
@@ -86,6 +106,33 @@ void getInput()
 void handleChange()
 {
     printf("CHANGE between %d and %d\n", i1, i2);
+
+    if (!head)
+        head = initializeLineHistory(1);
+
+    LineHistory *currentLine = head;
+
+    for (int k = 2; k < i1; ++k)
+    {
+        if (currentLine->nextLine == NULL)
+            currentLine->nextLine = initializeLineHistory(k);
+        currentLine = currentLine->nextLine;
+    }
+
+    for (int k = i1; k <= i2; ++k)
+    {
+        if (currentLine->nextLine == NULL)
+            currentLine->nextLine = initializeLineHistory(k);
+
+        currentLine->nextLine->currentVersion = malloc(sizeof(Line));
+        currentLine->nextLine->latestVersion = currentLine->nextLine->currentVersion;
+        gets(currentLine->nextLine->currentVersion->content);
+        currentLine->nextLine->currentVersion->prevVersion = currentLine->nextLine->currentVersion;
+        currentLine->nextLine->currentVersion->nextVersion = NULL;
+
+        currentLine = currentLine->nextLine;
+    }
+
     return;
 }
 
