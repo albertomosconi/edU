63fc0d0b22838403bc2cd4ca49387f552351ec57
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-07-29 19:08:31 +0200

    command history

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ struct Line
 };
 struct Command
 { /* command object to be stored in the history stack */
-    char type;
+    char c;
     int i1, i2;
     struct Command *prev; /* previous command in the stack*/
     struct Command *next; /* next command in the stack */
@@ -20,6 +20,7 @@ struct Command
 char raw_cmd[MAX_ROW_LENGTH]; /* command buffer */
 int i1, i2;                   /* line indexes specified in the command */
 char c;                       /* command character [q, c, d, p, u, r] */
+struct Command *command_history = NULL;
 
 /* MAIN FUNCTIONS */
 void getInput();     /* receive and parse input commands from stdin */
@@ -29,15 +30,18 @@ void handlePrint();  /* process print command */
 void handleUndo();   /* process undo command */
 void handleRedo();   /* process redo command */
 
-int main(int argc, char const *argv[])
+/* HELPER FUNCTIONS */
+void cmdToHistory();
+
+int main()
 {
     while (1)
     {
         getInput();
         switch (c)
         {
-        case 'q':
-            return 0;
+        case 'q': /* terminate the program */
+            exit(0);
         case 'c':
             handleChange();
             break;
@@ -54,30 +58,77 @@ int main(int argc, char const *argv[])
             handleRedo();
             break;
         default:
-            break;
+            exit(1);
         }
     }
     return 0;
 }
 
+void cmdToHistory()
+{ /* add the command to the history */
+    if (!command_history)
+    {
+        command_history = malloc(sizeof(struct Command));
+        command_history->prev = NULL;
+    }
+    else
+    {
+        command_history->next = malloc(sizeof(struct Command));
+        command_history->next->prev = command_history;
+        command_history = command_history->next;
+    }
+    command_history->c = c;
+    command_history->i1 = i1;
+    command_history->i2 = i2;
+    command_history->next = NULL;
+}
+
 void getInput()
 { /* receive and parse input commands from stdin */
+    fputs("> ", stdout);
     if (!fgets(raw_cmd, sizeof(raw_cmd), stdin))
         exit(1); /* if fail stop program */
 
     int success = sscanf(raw_cmd, "%d,%d%c", &i1, &i2, &c);
     if (1 == success)
+    {
         sscanf(raw_cmd, "%d%c", &i1, &c);
+        i2 = -1;
+    }
     else if (0 == success)
+    {
         sscanf(raw_cmd, "%c", &c);
+        i1 = -1;
+        i2 = -1;
+    }
 }
 
-void handleChange() {}
+void handleChange()
+{
+    cmdToHistory();
+}
 
-void handleDelete() {}
+void handleDelete()
+{
+    cmdToHistory();
+}
 
-void handlePrint() {}
+void handlePrint()
+{
+    struct Command *temp = command_history;
+    while (NULL != temp)
+    {
+        printf("%d,%d%c\n", temp->i1, temp->i2, temp->c);
+        temp = temp->prev;
+    }
+}
 
-void handleUndo() {}
+void handleUndo()
+{
+    printf("undo\n");
+}
 
-void handleRedo() {}
+void handleRedo()
+{
+    printf("redo\n");
+}
