ed28f43858b37091f32fcedc31fb7b51a66195cb
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-07-30 10:58:26 +0200

    better data structure

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX_ROW_LENGTH 1024
+#define MAX_ROW_LENGTH 1025
 
 /* STRUCTURES */
-struct Line
-{ /* single node of the linked list representing the document */
-    char content[MAX_ROW_LENGTH];
-    struct Line *next;
-};
 struct Command
 { /* command object to be stored in the history stack */
     char c;
     int i1, i2;
+    char **lines;         /* lines to be changed */
     struct Command *prev; /* previous command in the stack*/
     struct Command *next; /* next command in the stack */
+};
+struct Modifier
+{
+    int lim;               /* index above which the modifier must be applied */
+    int val;               /* number of deleted lines */
+    struct Modifier *next; /* next modifier */
 };
 
 /* VARIABLES */
@@ -21,6 +23,7 @@ char raw_cmd[MAX_ROW_LENGTH]; /* command buffer */
 int i1, i2;                   /* line indexes specified in the command */
 char c;                       /* command character [q, c, d, p, u, r] */
 struct Command *command_history = NULL;
+struct Modifier *mods = NULL;
 
 /* MAIN FUNCTIONS */
 void getInput();     /* receive and parse input commands from stdin */
@@ -32,33 +35,36 @@ void handleRedo();   /* process redo command */
 
 /* HELPER FUNCTIONS */
 void cmdToHistory();
+int max(int a, int b); /* returns maximum between a and b */
+int min(int a, int b); /* returns minimum between a and b */
+void updateIndexes();  /* applies all the modifiers to the indexes */
 
 int main()
 {
     while (1)
     {
-        getInput();
+        getInput(); /* fetch and parse input */
         switch (c)
         {
         case 'q': /* terminate the program */
-            exit(0);
-        case 'c':
+            return 0;
+        case 'c': /* change lines */
             handleChange();
             break;
-        case 'd':
+        case 'd': /* delete lines */
             handleDelete();
             break;
-        case 'p':
+        case 'p': /* print lines */
             handlePrint();
             break;
-        case 'u':
+        case 'u': /* undo commands */
             handleUndo();
             break;
-        case 'r':
+        case 'r': /* redo commands */
             handleRedo();
             break;
-        default:
-            exit(1);
+        default: /* unknown command */
+            break;
         }
     }
     return 0;
@@ -83,9 +89,27 @@ void cmdToHistory()
     command_history->next = NULL;
 }
 
+int max(int a, int b)
+{ /* returns maximum between a and b */
+    if (a >= b)
+        return a;
+    return b;
+}
+
+int min(int a, int b)
+{ /* returns minimum between a and b */
+    if (a > b)
+        return b;
+    return a;
+}
+
+void updateIndexes()
+{ /* applies all the modifiers to the indexes */
+}
+
 void getInput()
 { /* receive and parse input commands from stdin */
-    fputs("> ", stdout);
+    // fputs("> ", stdout);
     if (!fgets(raw_cmd, sizeof(raw_cmd), stdin))
         exit(1); /* if fail stop program */
 
@@ -105,22 +129,77 @@ void getInput()
 
 void handleChange()
 {
-    cmdToHistory();
+    updateIndexes(); /* update the indexes with the modifiers */
+
+    if (!command_history)
+    {
+        command_history = malloc(sizeof(struct Command));
+        command_history->prev = NULL;
+    }
+    else
+    {
+        command_history->next = malloc(sizeof(struct Command));
+        command_history->next->prev = command_history;
+        command_history = command_history->next;
+    }
+    command_history->c = c;
+    command_history->i1 = i1;
+    command_history->i2 = i2;
+    command_history->lines = malloc((i2 - i1 + 1) * sizeof(char *));
+    for (int k = 0; k < i2 - i1 + 1; ++k)
+    {
+        command_history->lines[k] = malloc(MAX_ROW_LENGTH * sizeof(char));
+        fgets(command_history->lines[k], MAX_ROW_LENGTH * sizeof(char), stdin);
+    }
+    command_history->next = NULL;
 }
 
 void handleDelete()
 {
-    cmdToHistory();
+    updateIndexes(); /* update the indexes with the modifiers */
+
+    /* add a modifier to the list */
 }
 
 void handlePrint()
 {
-    struct Command *temp = command_history;
-    while (NULL != temp)
+    char **buffer = malloc((i2 - i1 + 1) * sizeof(char *));
+    int *found = calloc((i2 - i1 + 1), sizeof(int));
+    int to_find = i2 - i1 + 1; /* how many elements are still to be found */
+    int len = to_find;
+
+    updateIndexes(); /* update the indexes with the modifiers */
+
+    struct Command *curr_cmd = command_history;
+    while ((curr_cmd != NULL) && (to_find > 0))
     {
-        printf("%d,%d%c\n", temp->i1, temp->i2, temp->c);
-        temp = temp->prev;
+        if ((i1 <= curr_cmd->i2) && (i2 >= curr_cmd->i1))
+        {
+            /* save lines from max(i1, curr_cmd->i1) to min(i2, curr_cmd->i2) */
+            for (int k = max(i1 - curr_cmd->i1, 0); k < min(i2, curr_cmd->i2) - curr_cmd->i1 + 1; ++k)
+            {
+                if (!found[k + (curr_cmd->i1 - i1)])
+                {
+                    buffer[k + (curr_cmd->i1 - i1)] = curr_cmd->lines[k];
+                    found[k + (curr_cmd->i1 - i1)] = 1;
+                    --to_find;
+                }
+            }
+        }
+        curr_cmd = curr_cmd->prev;
     }
+
+    /* print the buffer */
+    for (int k = 0; k < len; ++k)
+    {
+        if (!found[k])
+            fputs(".\n", stdout);
+        else
+            fputs(buffer[k], stdout);
+    }
+
+    free(buffer);
+    free(found);
 }
 
 void handleUndo()
