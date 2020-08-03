2f753e3f48d0d2fce647b1e7c94ad2cbf416101c
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-08-03 17:34:06 +0200

    almost working tree

--- main.c
+++ main.c
@@ -10,11 +10,30 @@ struct StringNode /* node in the strings tree */
     int color;                   /* node color, 1->red, 0->black */
     char string[MAX_ROW_LENGTH]; /* string contents */
 };
+struct Command
+{ /* command object to be stored in the history stack */
+    char c;
+    int i1, i2;
+    struct StringNode **lines; /* list of pointers to the lines in the tree */
+    struct Command *prev;      /* previous command in the stack*/
+    struct Command *next;      /* next command in the stack */
+};
+struct Modifier
+{
+    int lim;               /* index above which the modifier must be applied */
+    int val;               /* number of deleted lines */
+    struct Modifier *next; /* next modifier */
+    struct Modifier *prev; /* previous modifier */
+};
 /* GENERAL VARIABLES */
 char raw_cmd[MAX_ROW_LENGTH]; /* command buffer */
 int i1, i2;                   /* line indexes specified in the command */
 char c;                       /* command character [q, c, d, p, u, r] */
 struct StringNode *strings = NULL;
+struct Command *command_history = NULL;
+struct Command *actual_command = NULL;
+struct Modifier *mods = NULL;
+struct Modifier *actual_mod = NULL;
 /* MAIN FUNCTIONS */
 void getInput();     /* receive and parse input commands from stdin */
 void handleChange(); /* process change command */
@@ -36,7 +55,7 @@ struct StringNode *getSibling(struct StringNode *n); /* returns the sibling of a
 struct StringNode *getUncle(struct StringNode *n);   /* return the sibling of the parent of a given StringNode */
 void rotateLeft(struct StringNode *n);               /* rotates the string tree to the left */
 void rotateRight(struct StringNode *n);              /* rotates the string tree to the right */
-void insertString();
+struct StringNode *insertString();
 struct StringNode *treeInsert(struct StringNode *root, struct StringNode *n);
 void treeFixup(struct StringNode *n);
 
@@ -93,12 +112,78 @@ void getInput()
 }
 void handleChange()
 { /* process change command */
+    while (command_history != actual_command)
+    {
+        command_history = command_history->prev;
+        free(command_history->next);
+    }
+
+    updateIndexes(); /* update the indexes with the modifiers */
+
+    if (!command_history)
+    {
+        command_history = malloc(sizeof(struct Command));
+        command_history->prev = NULL;
+    }
+    else
+    {
+        command_history->next = malloc(sizeof(struct Command));
+        command_history->next->prev = command_history;
+        command_history = command_history->next;
+    }
+    command_history->c = c;
+    command_history->i1 = i1;
+    command_history->i2 = i2;
+    command_history->lines = malloc((i2 - i1 + 1) * sizeof(struct StringNode *));
+    for (int k = 0; k < i2 - i1 + 1; ++k)
+    {
+        command_history->lines[k] = insertString();
+    }
+    command_history->next = NULL;
+    actual_command = command_history;
 }
 void handleDelete()
 { /* procerss delete command */
 }
 void handlePrint()
 { /* process print command */
+    struct StringNode **buffer = malloc((i2 - i1 + 1) * sizeof(struct StringNode *));
+    int *found = calloc((i2 - i1 + 1), sizeof(int));
+    int to_find = i2 - i1 + 1; /* how many elements are still to be found */
+    int len = to_find;
+
+    updateIndexes(); /* update the indexes with the modifiers */
+
+    struct Command *curr_cmd = actual_command;
+    while ((curr_cmd != NULL) && (to_find > 0))
+    {
+        if ((i1 <= curr_cmd->i2) && (i2 >= curr_cmd->i1))
+        {
+            /* save lines from max(i1, curr_cmd->i1) to min(i2, curr_cmd->i2) */
+            for (int k = max(i1 - curr_cmd->i1, 0); k < min(i2, curr_cmd->i2) - curr_cmd->i1 + 1; ++k)
+            {
+                if (!found[k + (curr_cmd->i1 - i1)])
+                {
+                    buffer[k + (curr_cmd->i1 - i1)] = curr_cmd->lines[k];
+                    found[k + (curr_cmd->i1 - i1)] = 1;
+                    --to_find;
+                }
+            }
+        }
+        curr_cmd = curr_cmd->prev;
+    }
+
+    /* print the buffer */
+    for (int k = 0; k < len; ++k)
+    {
+        if (!found[k])
+            fputs(".\n", stdout);
+        else
+            fputs(buffer[k]->string, stdout);
+    }
+
+    free(buffer);
+    free(found);
 }
 void handleUndo()
 { /* process undo command */
@@ -128,6 +213,7 @@ int min(int a, int b)
 int minStr(char *a, char *b)
 { /* returns 1 if the string 'a' is < than 'b', otherwise 0 */
     int k = 0;
+
     while (a[k] != '\0')
     {
         if ((a[k] > b[k]) || (b[k] == '\0'))
@@ -136,16 +222,19 @@ int minStr(char *a, char *b)
     }
     return 1;
 }
-int maxStr(char *a, char *b)
-{ /* returns 1 if the string 'a' is > than 'b', otherwise 0 */
-    int k = 0;
-    while (b[k] != '\0')
+int strCmp(char *a, char *b)
+{ /* returns 0 if a == b, >0 if a>b, <0 if a<b */
+    char *s1 = (char *)a;
+    char *s2 = (char *)b;
+    char c1, c2;
+    do
     {
-        if ((a[k] < b[k]) || (a[k] == '\0'))
-            return 0;
-        ++k;
-    }
-    return 1;
+        c1 = (char)*s1++;
+        c2 = (char)*s2++;
+        if (c1 == '\0')
+            return c1 - c2;
+    } while (c1 == c2);
+    return c1 - c2;
 }
 struct StringNode *getParent(struct StringNode *n) /* returns the parent of a given StringNode */
 {
@@ -214,30 +303,32 @@ void rotateRight(struct StringNode *n) /* rotates the string tree to the right *
     n_left->right = n;
     n->parent = n_left;
 }
-void insertString()
+struct StringNode *insertString()
 {
-    struct StringNode newNode;
-    newNode.color = 1;
-    newNode.parent = NULL;
-    newNode.left = NULL;
-    newNode.right = NULL;
-    fgets(newNode.string, sizeof(newNode.string), stdin);
+    struct StringNode *newNode = malloc(sizeof(struct StringNode));
+    newNode->color = 1;
+    newNode->parent = NULL;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    fgets(newNode->string, sizeof(newNode->string), stdin);
 
-    strings = treeInsert(&newNode);
+    strings = treeInsert(strings, newNode);
 
-    treeFixup(&newNode);
+    treeFixup(newNode);
+
+    return newNode;
 }
 struct StringNode *treeInsert(struct StringNode *root, struct StringNode *n)
 {
-    if (strings == NULL)
+    if (root == NULL)
         return n;
 
-    if (minStr(n->string, root->string))
+    if (strCmp(n->string, root->string) < 0)
     {
         root->left = treeInsert(root->left, n);
         root->left->parent = root;
     }
-    else if (maxStr(n->string, root->string))
+    else if (strCmp(n->string, root->string) > 0)
     {
         root->right = treeInsert(root->right, n);
         root->right->parent = root;
@@ -247,4 +338,69 @@ struct StringNode *treeInsert(struct StringNode *root, struct StringNode *n)
 }
 void treeFixup(struct StringNode *n)
 {
+    struct StringNode *p = NULL;
+    struct StringNode *gp = NULL;
+
+    while ((n != strings) && (n->color != 0) && (n->parent->color == 1))
+    {
+        p = n->parent;
+        gp = p->parent;
+
+        if (p == gp->left)
+        { /* parent of n is left child of grand-parent of n */
+            struct StringNode *u = gp->right;
+
+            if ((u != NULL) && (u->color == 1))
+            { /* if uncle of n is red*/
+                gp->color = 1;
+                p->color = 0;
+                u->color = 0;
+                n = gp;
+            }
+            else
+            {
+                if (n == p->right)
+                {
+                    rotateLeft(p);
+                    n = p;
+                    p = p->parent;
+                }
+
+                rotateRight(gp);
+                int temp = p->color;
+                p->color = gp->color;
+                gp->color = temp;
+                n = p;
+            }
+        }
+        else
+        { /* parent of n is right child of grandparent of n */
+            struct StringNode *u = gp->left;
+
+            if ((u != NULL) && (u->color == 1))
+            { /* if uncle of n is red*/
+                gp->color = 1;
+                p->color = 0;
+                u->color = 0;
+                n = gp;
+            }
+            else
+            {
+                if (n == p->left)
+                {
+                    rotateRight(p);
+                    n = p;
+                    p = p->parent;
+                }
+
+                rotateLeft(gp);
+                int temp = p->color;
+                p->color = gp->color;
+                gp->color = temp;
+                n = p;
+            }
+        }
+    }
+
+    strings->color = 0;
 }
