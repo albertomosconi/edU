33312f4220dd670a554f64b4673bbeec7af24d2b
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-08-05 18:18:08 +0200

    working tree insert i think

--- main.c
+++ main.c
@@ -56,7 +56,7 @@ struct StringNode *getUncle(struct StringNode *n);   /* return the sibling of th
 void rotateLeft(struct StringNode *n);               /* rotates the string tree to the left */
 void rotateRight(struct StringNode *n);              /* rotates the string tree to the right */
 struct StringNode *insertString();
-struct StringNode *treeInsert(struct StringNode *root, struct StringNode *n);
+struct StringNode *treeInsert(struct StringNode *root, struct StringNode *n, struct StringNode **s);
 void treeFixup(struct StringNode *n);
 
 int main()
@@ -304,7 +304,7 @@ void rotateRight(struct StringNode *n) /* rotates the string tree to the right *
     n->parent = n_left;
 }
 struct StringNode *insertString()
-{
+{ /* gets new line from stdin and inserts in memory (if it doesn't exist already) and returns a pointer to it */
     struct StringNode *newNode = malloc(sizeof(struct StringNode));
     newNode->color = 1;
     newNode->parent = NULL;
@@ -312,28 +312,39 @@ struct StringNode *insertString()
     newNode->right = NULL;
     fgets(newNode->string, sizeof(newNode->string), stdin);
 
-    strings = treeInsert(strings, newNode);
+    struct StringNode *string = NULL;
+    strings = treeInsert(strings, newNode, &string);
 
-    treeFixup(newNode);
+    if (string == newNode)
+        treeFixup(newNode);
 
-    return newNode;
+    return string;
 }
-struct StringNode *treeInsert(struct StringNode *root, struct StringNode *n)
-{
+struct StringNode *treeInsert(struct StringNode *root, struct StringNode *n, struct StringNode **s)
+{ /* if it doesnt exist, insert n in the strings tree, save pointer to it in s */
     if (root == NULL)
-        return n;
-
-    if (strCmp(n->string, root->string) < 0)
     {
-        root->left = treeInsert(root->left, n);
+        if (*s == NULL)
+            *s = n;
+        return n;
+    }
+
+    int cmp = strCmp(n->string, root->string);
+    if (cmp < 0)
+    {
+        root->left = treeInsert(root->left, n, s);
         root->left->parent = root;
     }
-    else if (strCmp(n->string, root->string) > 0)
+    else if (cmp > 0)
     {
-        root->right = treeInsert(root->right, n);
+        root->right = treeInsert(root->right, n, s);
         root->right->parent = root;
     }
-
+    else 
+    {
+        free(n);
+        *s = root;
+    }
     return root;
 }
 void treeFixup(struct StringNode *n)
@@ -365,7 +376,6 @@ void treeFixup(struct StringNode *n)
                     n = p;
                     p = p->parent;
                 }
-
                 rotateRight(gp);
                 int temp = p->color;
                 p->color = gp->color;
@@ -392,7 +402,6 @@ void treeFixup(struct StringNode *n)
                     n = p;
                     p = p->parent;
                 }
-
                 rotateLeft(gp);
                 int temp = p->color;
                 p->color = gp->color;
@@ -401,6 +410,5 @@ void treeFixup(struct StringNode *n)
             }
         }
     }
-
     strings->color = 0;
 }
